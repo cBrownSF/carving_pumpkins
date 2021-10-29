4010b834189fe9dfe92d56424b1864ad6e52cba0
Cal Brown <calsbrown4@gmail.com>
2021-10-29 11:43:54 -0700

    Test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Example from "./scripts/example";
+
+document.addEventListener("DOMcontentLoaded", () =>{ 
+console.log("hello world");
+const main = document.getElementById("main");
+new Example(main)
+})
