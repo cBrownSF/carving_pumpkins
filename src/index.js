0cdf702267b91566f6308acf2a54c17d4b3b51b4
Cal Brown <calsbrown4@gmail.com>
2021-10-29 11:59:44 -0700

    Carving Pumpkins

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import Example from "./scripts/example";
 document.addEventListener("DOMcontentLoaded", () =>{ 
 console.log("hello world");
 const main = document.getElementById("main");
-new Example(main)
+new Example(main);
 })
