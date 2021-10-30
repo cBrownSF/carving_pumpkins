5442499c9475a5e86b048d97076cdd5376d88f7c
Cal Brown <calsbrown4@gmail.com>
2021-10-30 14:23:40 -0700

    First Edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,64 @@
 import Example from "./scripts/example";
 
-document.addEventListener("DOMContentLoaded", () =>{ 
-console.log("hello world");
-const main = document.getElementById("main");
-new Example(main);
-})
+// document.addEventListener("DOMContentLoaded", () =>{ 
+// console.log("hello world");
+// const main = document.getElementById("main");
+// new Example(main);
+// })
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+//fillRect
+// ctx.fillRect(20,60,150,100);
+// ctx.fillStyle = 'black';
+
+// //strokeRect
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = 'green'
+// ctx.strokeRect(100,200,150,150);
+// //clearRect
+// ctx.clearRect(25,25,140,90)
+// //strokeText
+// ctx.fillText('Pumpkins',400,50)
+// ctx.font = '50px Times'
+// ctx.strokeText('Pumpkins',400,100)
+//stroke text referenes line width and stroke style from above
+
+///Paths--triangular buttons like candy corn
+// ctx.beginPath()
+// ctx.moveTo(50,50);
+// ctx.lineTo(150,50);
+// ctx.lineTo(100,200);
+// ctx.stroke()
+// ctx.closePath();
+
+//Arcs->
+// ctx.arc(canvas.width/2, canvas.height / 2,40,0,Math.Pi *2)
+// ctx.stroke();
+// ctx.moveTo()
+//Add image and then we can move on this
+function move(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  candyCorn.x 
+}
+const candyImg = document.getElementById("corn");
+
+const candyCorn = {
+  w: 50,
+  h: 69,
+  x:40,
+  y: 150,
+  speed: 3,
+  dx: 4,
+  dy: 4
+}
+
+function drawCorn(){
+  ctx.drawImage(candyImg, candyCorn.x, candyCorn.y, candyCorn.w, candyCorn.h)
+}
+
+function animate(){
+  drawCorn();
+  requestAnimationFrame(animate);
+
+}
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,14 +1,14 @@
-class Example {
-  constructor(ele){
-    this.ele = ele;
-    this.ele.innerHTML = "<h1>I'm Alive!</h1>";
-    this.handClick = this.handleClick.bind(this);
-    this.ele.addEventListener("click", this.handleClick)
-  }
+// class Example {
+// // //   constructor(ele){
+// //     this.ele = ele;
+// //     this.ele.innerHTML = "<h1>I'm Alive!</h1>";
+// //     this.handClick = this.handleClick.bind(this);
+// //     this.ele.addEventListener("click", this.handleClick)
+// //   }
 
-  handleClick(){
-    this.ele.children[0].innerText = "Ouch!";
-  }
-}
+// //   handleClick(){
+// //     this.ele.children[0].innerText = "Ouch!";
+// //   }
+// // }
 
-export default Example;
+// export default Example;
