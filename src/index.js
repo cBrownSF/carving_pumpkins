fda08d016dafa43f6c11372bff16994fe5c1b396
Cal Brown <calsbrown4@gmail.com>
2021-10-30 15:51:02 -0700

    edit

--- src/index.js
+++ src/index.js
@@ -1,85 +1,45 @@
 // import Example from "./scripts/example";
 
-// document.addEventListener("DOMContentLoaded", function () {
-// const canvas = document.getElementById("mycanvas");
-// const ctx = canvas.getContext('2d');
-// ctx.fillStyle = "red";
-// ctx.fillRect(0, 100, 300, 300);
-// // /fillRect
-// // ctx.fillRect(20,60,150,100);
-// // ctx.fillStyle = 'black';
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("mycanvas");
+  canvas.width = 800;
+  canvas.height = 600;
+  const ctx = canvas.getContext('2d');
+  const candyImg = document.getElementById("corn");
+  
 
-// // //strokeRect
-// // ctx.lineWidth = 5;
-// // ctx.strokeStyle = 'green'
-// // ctx.strokeRect(100,200,150,150);
-// // //clearRect
-// // ctx.clearRect(25,25,140,90)
-// // //strokeText
-// // ctx.fillText('Pumpkins',400,50)
-// // ctx.font = '50px Times'
-// // ctx.strokeText('Pumpkins',400,100)
-// //stroke text referenes line width and stroke style from above
+// image.onload = function(){
 
-// /Paths--triangular buttons like candy corn
-// ctx.beginPath()
-// ctx.moveTo(50,50);
-// ctx.lineTo(150,50);
-// ctx.lineTo(100,200);
-// ctx.stroke()
-// ctx.closePath();
 
-// //Arcs->
-// // ctx.arc(canvas.width/2, canvas.height / 2,40,0,Math.Pi *2)
-// // ctx.stroke();
-// // ctx.moveTo()
-// //Add image and then we can move on this
-// const candyImg = document.getElementById("corn");
+const candyCorn = {
+  width: 50,
+  height: 69,
+  x:0,
+  y: 0,
+  speed: 3,
+  dx: 0,
+  dy: 0
+}
+function drawCorn() {
+    ctx.drawImage(candyImg, candyCorn.x, candyCorn.y, candyCorn.width, candyCorn.height)
+  }
+// function animate(){
+//   ctx.clearRect(0,0,canvas.width, canvas.height)
+//   drawCorn();
+//   // requestAnimationFrame(animate);
+//   // candyCorn.x += candyCorn.dx;
+//   // candyCorn.x += candyCorn.dx;
+// }
+
 // function move(){
 //   ctx.clearRect(0,0,canvas.width, canvas.height);
 //   candyCorn.x 
 // }
-
-
-// const candyCorn = {
-//   w: 50,
-//   h: 69,
-//   x:40,
-//   y: 150,
-//   speed: 3,
-//   dx: 4,
-//   dy: 4
+// function resizeCanvas(){
+//   canvas.width = 800;
+//   canvas.height = 600;
 // }
-
-// function drawCorn(){
-//   ctx.drawImage(candyImg, candyCorn.x, candyCorn.y, candyCorn.w, candyCorn.h)
-// }
-
-// function animate(){
-//   ctx.clearRect(0,0,canvas.width, canvas.height)
-//   drawCorn();
-//   requestAnimationFrame(animate);
-//   candyCorn.x += candyCorn.dx;
-//   candyCorn.x += candyCorn.dx;
-// }
-// });
-
-// const canvas = document.getElementById("mycanvas");
-// const canvas = document.getElementById("mycanvas");
-// ctx.fillStyle = "red";
-// ctx.fillRect(0, 100, 300, 300);
-document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.getElementById("mycanvas");
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 100, 300, 300);
-
-  // ctx.beginPath();
-  // ctx.arc(200, 60, 50, 0, -75, 75);
-  // ctx.strokeStyle = 'green';
-  // ctx.lineWidth = 5;
-  // ctx.stroke();
-  // ctx.fillStyle = 'green';
-  // ctx.fill();
-
+// window.addEventListener('resize', function(){
+//   resizeCanvas();
+// })
 });
