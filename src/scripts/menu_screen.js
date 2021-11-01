c754c1aca278050ba7bda6dc23ecb05c54794095
Cal Brown <calsbrown4@gmail.com>
2021-11-01 00:58:06 -0700

    Populate

--- src/scripts/menu_screen.js
+++ src/scripts/menu_screen.js
@@ -16,13 +16,27 @@ import CandyCorn from './candy_corn.js'
     for (let i = 0; i < this.NUM_CANDY; i++) {
       const canvas = document.getElementById("mycanvas");
       const ctx = canvas.getContext('2d');
-      let x = Math.random() * canvas.width
-      let y = Math.random() * canvas.height
+      let x = Math.random() * canvas.width;
+      let y = Math.random() * canvas.height;
+      if (i !== 0){
+        for (let j = 0; j<this.candyArray.length; j++) {
+          if (this.distBetween(x1, y1, candyArray[j].x, candyArray[j].y) <=0){
+            x = Math.random() * canvas.width;
+            y = Math.random() * canvas.height;
+            //also can possibly change the value here later so that they are spaced farther apart
+            //still an issue because these newCoordinates could also already be filled
+          }
+        }
+      }
       let corn = new CandyCorn(x,y)
       this.candyArray.push(corn);
       }
   }
- 
+   distBetween(x1, y1, x2, y2) {
+     return Math.sqrt(
+       Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)
+     );
+   }
   drawCandy(){
     for (let i = 0; i < this.candyArray.length; i ++){
   
