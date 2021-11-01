818aba8e8752bde980b4f2f6f78db7bb88df096b
Cal Brown <calsbrown4@gmail.com>
2021-11-01 00:38:30 -0700

    Edit

--- src/scripts/candy_corn.js
+++ src/scripts/candy_corn.js
@@ -1,10 +1,12 @@
+import { throws } from "assert";
+
 class CandyCorn {
  
   constructor(x,y){
     this.x = x;
     this.y = y;
-    this.dx = 5;
-    this.dy = 4;
+    this.velX = 5;
+    this.velY = 4;
      const canvas = document.getElementById("mycanvas");
     const ctx = canvas.getContext('2d');
     this.drawCorn(ctx)
@@ -17,16 +19,29 @@ class CandyCorn {
       ctx.drawImage(candyImg, this.x, this.y);
     }
 
-  moveCorn(ctx){
-    const canvas = document.getElementById("mycanvas");
-    // const ctx = canvas.getContext('2d');
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    this.drawCorn(ctx);
-    let newX = this.x + this.dx
-    let newY = this.y + this.dy;
-    this.x = newX;
-    this.y = newY;
-    requestAnimationFrame(this.moveCorn);
-  }
+  // moveCorn(ctx){
+  //   const canvas = document.getElementById("mycanvas");
+  //   // // const ctx = canvas.getContext('2d');
+  //   //ctx.clearRect(0,0,canvas.width,canvas.height)
+  //   this.drawCorn(ctx);
+  //   let newX = this.x + this.velX
+  //   let newY = this.y + this.velY;
+  //   this.x = newX;
+  //   this.y = newY;
+  //   requestAnimationFrame(this.moveCorn(ctx));
+  // }
+  //checkWalls(){
+    //if (if (this.x > canvas.width || corn.x <0){
+//     this.velX = (this.velX * -1)
+// }
+// if (corn.y > canvas.height || corn.y < 0) {
+//   this.velY = (corn.dy * -1)
+// })
+  //}
+  // isOnTitle(){
+    //example
+  //   if (this.x > 200 || this.y < 600)
+  //     this.velX = (this.velX * -1)
+  // }
 } 
 export default CandyCorn
---
+++ src/scripts/util.js
@@ -0,0 +1,18 @@
+const Util = {
+  getDist(x1,y1, x2,y2) {
+    return Math.sqrt(
+      Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)
+    );
+  },
+  // isCollided(getDist){
+  //  return getDist === 0;
+  // }
+
+//if (isCollided === true){
+  // this.velX = velX * -1
+    // this.velY = velY * -1
+// } else{
+  // this.velX;
+  // this.velY;
+// }
+}
