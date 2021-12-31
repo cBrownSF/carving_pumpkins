7d09cb43fc350d241df139a0d1199fe328aaf20f
Cal Brown <calsbrown4@gmail.com>
2021-12-31 11:18:11 -0800

    Loading issue

--- src/scripts/instructions.js
+++ src/scripts/instructions.js
@@ -17,35 +17,31 @@ class Instructions {
     // let button = new Button(this.canvas.width/2, this.canvas.height/2 +300, 70, "START!",this.canvas)
     let ctx = this.ctx
     let canvas = this.canvas
-    let offX = ctx.canvas.offsetLeft;
-    let offY = ctx.canvas.offsetTop;
-    // this.ctx.beginPath()
     let button = new Path2D()
     button.arc(this.canvas.width / 2, this.canvas.height / 2 + 300, 70, 0, Math.PI * 2, true);
     button.lineWidth = 5;
     this.ctx.fillStyle = "#E66C2C";
     this.ctx.fill(button);
-    // this.ctx.stroke()
+    this.ctx.font = '35pt Impact'
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = 'center';
+    this.ctx.fillText("Start", this.canvas.width / 2, this.canvas.height / 2 + 315)
+    this.ctx.stroke(button)
 
-    // button.fillStyle = "#E66C2C";
-    // button.fill()
-    // button.stroke()
-  //  button.textAlign = "center";
-  //  button.fillText("START!", this.canvas.width/2,this.canvas.height/2 + 315)
-  //   button.stroke()
-    //helllo
     this.startButton = button;
 
     canvas.addEventListener('mousemove', function (event) {
       if (ctx.isPointInPath(button, event.offsetX, event.offsetY)) {
         this.hovered = true;
-        console.log(this.hovered)
+       console.log(this.hovered)
       }
       else {
         this.hovered = false
         console.log(this.hovered)
       }
     })
+    
+    // ctx.fill(button)
   }
 
   loadGameScreen() {
@@ -55,22 +51,19 @@ class Instructions {
     let canvas = this.canvas
     let offX = ctx.canvas.offsetLeft;
     let offY = ctx.canvas.offsetTop;
+    
 
 
     let rect = canvas.getBoundingClientRect(), // 
       scaleX = canvas.width / rect.width,
       scaleY = canvas.height / rect.height;
-    console.log(offX)
-    console.log(rect.left)
-    console.log(rect.top)
-    console.log(offY)
+  
     canvas.addEventListener('click', function (e) {
       let mouseX = (e.clientX - rect.left) * scaleX
       let mouseY = (e.clientY - rect.top) * scaleY
-      console.log(`buttonX:${button.x},MX:${mouseX}`)
-      console.log(`buttonY:${button.y}, MY:${mouseY}`)
+      
 
-      if ((mouseX >= button.x - 59 && mouseX < button.x + 59) && (mouseY > button.y - 59 && mouseY < button.y + 45) && clickArray.includes('instruct')) {
+      if (this.hovered === true && clickArray.includes('instruct')) {
 
         debugger;
         ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -79,35 +72,11 @@ class Instructions {
       } else {
         return false
       }
-
-
     })
+
   }
 
-  // newone() {
-  //   const mouseTrack = {
-  //     x: null,
-  //     y: null
-  //   }
-  //   mouseTrack.x = e.clientX
-  //   mouseTrack.y = e.clientY
-  //   // console.log(button)
-  //   console.log(this.startButton)
 
-
-  //   console.log(`canvas width needed: ${canvas.width / 2 - 60} - ${canvas.width / 2 + 45}`)
-  //   console.log(`canvas height needed: ${canvas.height / 2 + 200 - 42}-${canvas.height / 2 + 200 + 45}`)
-  //   console.log(`mousePosX:${mouseTrack.x},mousePosY:${mouseTrack.y}`)
-  //   console.log('-----------------------------------')
-
-  //   // if ((mouseTrack.x >= canvas.width / 2 - 60 && mouseTrack.x < canvas.width / 2 + 45) && (mouseTrack.y > canvas.height / 2 + 200 - 42 && canvas.height / 2 + 200 + 45) && clickArray.includes('instruct')) {
-  //   //   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  //   //   clickArray.splice(0, 1)
-  //   //   new GameScreen(canvas)
-  //   // } else {
-  //   //   return false
-  //   // }
-  // }
   drawBox() {
     let instructions = '                         ----GAMEPLAY----?1. Click and hold the mouse to begin carving?2. Carve the pumpkin by moving the mouse?3. Move the black line back to the starting point?4. Let go of the mouse to complete the carve ?5. Repeat the process and keep carvin!'
     let x = this.canvas.width / 2 - 450;
